aec31116f18612db5b2217750870de9e0c6b84fc
Carlier Jacques <125122744+jacquescarlier@users.noreply.github.com>
2025-09-29 19:00:34 +0200

    Update About.jsx

--- src/containers/about/About.jsx
+++ src/containers/about/About.jsx
@@ -6,11 +6,16 @@ const About = () => {
       <h2 className=" text-center text-2xl lg:text-4xl  text-font1 pb-5 pt-2 font-font-secondary tracking-[0.5em] hover:animate-textRotation hover:text-black" id="about">
         About
       </h2>
-      <p className="aboutSection w-full lg:text-2xl md:text-xl sm:text-[16px]   bg-transparent text-font1 p-[15px_1em_0.5em_1em] mx-auto -my-[10px] mb-[60px] rounded-[25px] font-font-main font-semibold hover:text-bg-color hover:border-2 hover:border-neutral-color hover:shadow-[5px_5px_5px_var(--neutral-color">{aboutData[0].content}</p>
+     <div className="aboutSection w-full lg:text-2xl md:text-xl sm:text-[16px]   bg-transparent text-font1 p-[15px_1em_0.5em_1em] mx-auto -my-[10px] mb-[6px] rounded-[25px] font-font-main font-semibold hover:text-bg-color hover:border-2 hover:border-neutral-color hover:shadow-[5px_5px_5px_var(--neutral-color">
+        <p>{aboutData[0].content}</p>
+        <p>{aboutData[1].content}</p>
+        <p>{aboutData[2].content}</p>
+      </div>
     </>
 
   );
 };
 export default About;
+
 
 
